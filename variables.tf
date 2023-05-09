61cc646c6916a5630e5b1acb3bb58874ecef271b
Racheal <raeabbey7643@gmail.com>
2023-05-09 13:49:02 -0700

    uploading my terraform template

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "region" {
+  type        = string
+  default     = ""
+  description = "Name of the region"
+}
+
+variable "most_recent_ami" {
+  type        = bool
+  default     = true
+  description = "Fetch the most recent AMI"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  default     = []
+  description = "The list of available availability zones in the AWS region."
+}
+
+# variable "tags" {
+#   type        = string
+#   default     = ""
+#   description = "The name tag for the EC2 instance"
+# }
+
+variable "instance_type" {
+  type        = string
+  default     = ""
+  description = "instance type for our ec2 instance"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidr" {
+  type        = list(string)
+  default     = []
+  description = "CIDR block for my first public subnet"
+}
+
+
+variable "private_subnet_cidr" {
+  type        = list(string)
+  default     = []
+  description = "CIDR block for my first private subnet"
+}
